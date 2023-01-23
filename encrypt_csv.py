c282a56062ec7a86cb7d899dbfce7f932d73957f
KachinaDrive <123350910+KachinaDrive@users.noreply.github.com>
2023-01-23 08:25:34 -0800

    Add files via upload

---
+++ encrypt_csv.py
@@ -0,0 +1,59 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import os
+from cryptography.hazmat.primitives.padding import PKCS7
+
+from datetime import datetime
+
+
+
+with open("public_key.pem", "rb") as f:
+    public_key = serialization.load_pem_public_key(
+        f.read(),
+        backend=default_backend()
+    )
+
+# generate a random AES key
+aes_key = os.urandom(32)
+iv = os.urandom(16)
+
+file_csv = input("full name of csv file to encrypt: ")
+with open(file_csv, "r") as f:
+    data = f.read()  
+
+cipher = Cipher(algorithms.AES(aes_key), modes.CBC(iv), backend=default_backend())
+encryptor = cipher.encryptor()
+
+# Padding the plaintext
+padder = PKCS7(128).padder()
+padded_data = padder.update(data) + padder.finalize()
+ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+now = datetime.now()
+dt_string = now.strftime("%m-%d-%Y at %H-%M-%S")
+
+
+file_name = dt_string+".csb"
+
+
+# Encrypt the AES key with the RSA public key
+rsa_cipher = public_key.encrypt(
+    aes_key,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+    
+    
+with open(file_name, "wb") as f:
+    f.write(iv)
+    f.write(rsa_cipher)
+    f.write(ciphertext)
+    
+
+
